0f6c0b8d1d2ce0685631f478d64b098a7b37bc14
David <defaultik.personal@gmail.com>
2024-09-05 20:00:53 +0300

    Code optimization

--- source/main.py
+++ source/main.py
@@ -1,50 +1,39 @@
+import os
+from ctypes import windll
 from network import *
 from hardware import *
 
 
+def init():
+    if os.name == "nt":
+        if windll.shell32.IsUserAnAdmin():
+            main()
+        else:
+            print("ERROR: Program launched without admin rights\nPlease run CMD/Terminal as an administrator")
+    else:
+        print("ERROR: This script supports only Windows systems")
+
+
 def main():
-    print("INFO: Validating TPM...")
-    if TPM.validation():
-        print("INFO: TPM successfully validated\n")
-    else:
-        print("WARNING: TPM is Disabled, but your computer supports it\nStrongly recommended to turn it on in BIOS/UEFI\n")
+    for i in (TPM(), SecureBoot()):
+        print("INFO: Validating " + i.name)
 
-    print("INFO: Validating Secure Boot...")
-    if SecureBoot.validation():
-        print("INFO: Secure Boot successfully validated\n")
-    else:
-        print("WARNING: Secure Boot is Disabled, but your computer supports it\nStrongly recommended to turn it on in BIOS/UEFI\n")
+        if i.validation():
+            print(f"INFO: {i.name} successfully validated\n")
+        else:
+            print(f"WARNING: {i.name} is Disabled, but your computer supports it\nStrongly recommended to turn it on in BIOS/UEFI\n")
 
-    print("INFO: Trying to disable SMBv1...")
-    if SMBv1.status():
-        print("INFO: SMBv1 already disabled\n")
-    else:
-        SMBv1.disable()
-        if SMBv1.status():
-            print("INFO: SMBv1 successfully disabled\n")
+    for i in (SMBv1(), LLMNR(), NetBIOS()):
+        print(f"INFO: Trying to disable {i.name}...")
+        if i.status():
+            print(f"INFO: {i.name} already disabled\n")
         else:
-            print("WARNING: Failed to disable SMBv1\n")
-
-    print("INFO: Trying to disable LLMNR...")
-    if LLMNR.status():
-        print("INFO: LLMNR already disabled")
-    else:
-        LLMNR.disable()
-        if LLMNR.status():
-            print("INFO: LLMNR successfully disabled")
-        else:
-            print("WARNING: Failed to disable LLMNR")
-    
-    print("INFO: Trying to disable NetBIOS...")
-    if NetBIOS.status():
-        print("INFO: NetBIOS already disabled")
-    else:
-        NetBIOS.disable()
-        if NetBIOS.status():
-            print("INFO: NetBIOS successfully disabled")
-        else:
-            print("WARNING: Failed to disable NetBIOS")
+            i.disable()
+            if i.status():
+                print(f"INFO: {i.name} successfully disabled\n")
+            else:
+                print(f"WARNING: Failed to disable {i.name}\n")
 
 
 if __name__ == "__main__":
-    main()
+    init()
